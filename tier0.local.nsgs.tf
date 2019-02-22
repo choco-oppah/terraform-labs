4fef8db19fda3b99a6c8f243a8d547ce0431abe2
Nathan Nash <nnash@microsoft.com>
2019-02-23 00:29:45 +0800

    Added RDP to NSG rules

--- tier0.local.nsgs.tf
+++ tier0.local.nsgs.tf
@@ -90,3 +90,37 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         destination_address_prefix = "*"
   }
 }
+
+resource "azurerm_network_security_group" "nic_windows" {
+   name = "NIC_Windows"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name                       = "RDP"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 3389
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+
+    priority                    = 1030
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
